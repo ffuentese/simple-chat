f7eb9015229311fef8c992cefec87c574cfd56ff
ffuentese <ffuentese@users.noreply.github.com>
2018-03-29 00:53:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var http = require(‘http’),
 fs = require(‘fs’),
 server = require('http').createServer(app),
 io = require('socket.io').listen(server),
-server.listen(process.env.PORT || 3000);
 index;
 fs.readFile(‘./chat.html’, function (err, data) {
  if (err) {
@@ -10,4 +9,4 @@ fs.readFile(‘./chat.html’, function (err, data) {
  }
  index = data;
 });
-
+server.listen(process.env.PORT || 3000);
